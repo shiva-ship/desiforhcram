96f2906e4fb40de0d57420fb04798fe51d7c897f
Rastosh <shivansh.rastogi19@vit.edu>
2022-10-29 01:43:39 +0530

    backend added - without funcs

---
+++ Backend-v-1/src/main/java/com/example/Backendv1/BackendV1Application.java
@@ -0,0 +1,13 @@
+package com.example.Backendv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendV1Application.class, args);
+	}
+
+}
---
+++ Backend-v-1/src/test/java/com/example/Backendv1/BackendV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backendv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
