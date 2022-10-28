fcbc9cd44b8a265f1ff82b84e82526cec88da4f4
Rastosh <shivansh.rastogi19@vit.edu>
2022-10-29 02:40:37 +0530

    adding backend try 2

---
+++ Backend-v-2/src/test/java/com/example/Backendv2/BackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backendv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
